8a2854f25f8d68f48cfe96be5c3d574cf46c45b1
arianna-distefano <s4094442@student.rmit.edu.au>
2026-03-16 15:39:55 +1100

    Feedback Design Experiment

---
+++ Feedback Design Experiment/main.js
@@ -0,0 +1,35 @@
+//find elements to use
+const introDialog = document.getElementById("introDialog");
+const dialogCloseButton = document.getElementById("closeDialog");
+
+//intro dialog set up
+introDialog.showModal();
+
+dialogCloseButton.addEventListener("click",closeDialog);
+
+function closeDialog() {}
+introDialog.close();
+Tone.start();
+}
+
+//tone synth init
+const synth = new Tone.synth().toDestination():
+
+)
+
+//play sound with tone
+function playNote(){
+    synth.triggerAttackRelease("c4","8n");
+}
+//playButton.addEventListener("click",playNote);
+
+function startNote(){
+    synth.triggerAttack("c4");
+}
+
+function endNote(){
+    synth.triggerRelease();
+}
+
+playButton.addEventListener("mousedown",startNote);
+playButton.addEventListener("mouseup",endNote);
